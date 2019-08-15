10149ef9db847eed122d41f075ba12565565ec22
Martin Swiderski <martinswiderski@googlemail.com>
2019-08-15 17:03:11 +0100

    refactored implementation to cater for non-numbers in input values

--- lesson-01-tdd-primer/code-and-test.js
+++ lesson-01-tdd-primer/code-and-test.js
@@ -8,10 +8,13 @@ var color = require('bash-color');
  * @param numeric net     Net value - base for VAT calculation
  * @param numeric vatRate VAT rate expressed as decimal e.g. 0.15 for 15%
  *
- * @return numeric
+ * @return numeric|false
  */
 function calculateVat(net, vatRate) {
-    return net*vatRate;
+    if (typeof net === 'number' && typeof vatRate === 'number') {
+        return net*vatRate;
+    }
+    return false;
 }
 
 
@@ -46,6 +49,11 @@ var test = {
         value: -100,
         rate: 0.22,
         expected: -22
+    },
+    no_number: {
+        value: new Array(1, 2, 3, 4),
+        rate: 0.22,
+        expected: false
     }
 };
 
@@ -61,6 +69,11 @@ runTest(
 runTest(
     (calculateVat(test.negative.value, test.negative.rate) === test.negative.expected),  // espression
         "For positive value of -100 and rate 22% the VAT is -22"                         // message
+);
+
+runTest(
+    (calculateVat(test.no_number.value, test.no_number.rate) === test.no_number.expected),  // espression
+        "For positive value of 'Array' (non-number) and rate 22% the VAT is not calculated [false] returned"                         // message
 );
 
 
